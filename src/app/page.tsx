7f39ddd88a0dc2e19de0dd91180e32e8e11cce79
Ryan Martin <rmrt1n@users.noreply.github.com>
2023-10-09 17:12:39 +0700

    feat: groundwork for shareable links to scan qr codes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <p>hi world</p>
-      <Link href="/sign-in">sign in here</Link>
+      <Link href="/events">sign in here</Link>
     </main>
   )
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -9,11 +9,25 @@ export const middleware = async (req: NextRequest) => {
     data: { user },
   } = await supabase.auth.getUser()
 
-  if (!user && req.nextUrl.pathname.startsWith("/events")) {
+  const { pathname, searchParams } = req.nextUrl
+  const scanToken = searchParams.get("scanToken")
+
+  // TODO: find a way to create time-bound token on demmand & validate here
+  if (
+    !user &&
+    pathname.match(/\/events\/[^\/]+\/scan/) &&
+    scanToken &&
+    scanToken === "timebound-unique-shared-token"
+  ) {
+    console.log("valid token")
+    return res
+  }
+
+  if (!user && pathname.startsWith("/events")) {
     return NextResponse.redirect(new URL("/sign-in", req.url))
   }
 
-  if (user && req.nextUrl.pathname.startsWith("/sign-in")) {
+  if (user && pathname.startsWith("/sign-in")) {
     return NextResponse.redirect(new URL("/events", req.url))
   }
 
