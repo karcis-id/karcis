c2cb513cebbdbb76658227d1f893cb607927db00
Ryan Martin <rmrt1n@users.noreply.github.com>
2023-10-12 18:10:52 +0700

    feat: fetch events from db & separate event card component

--- src/app/(app)/events/page.tsx
+++ src/app/(app)/events/page.tsx
@@ -1,21 +1,28 @@
-import { NextPage } from "next"
 import Link from "next/link"
+import { cookies } from "next/headers"
 import { PlusCircledIcon } from "@radix-ui/react-icons"
 
-import { Badge } from "@/components/ui/badge"
 import { buttonVariants } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { cn } from "@/lib/utils"
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+import { EventCard } from "@/components/event-card"
+import { Database } from "@/lib/types/supabase"
 
-const Events: NextPage = () => {
+const getEvents = async () => {
+  const supabase = createServerComponentClient<Database>({ cookies })
+  const { data: events, error } = await supabase.from("events").select()
+
+  console.log("fetched: ", events)
+  if (error) {
+    console.log(error)
+  }
+
+  return events
+}
+
+const Events = async () => {
+  const events = await getEvents()
+
   // TODO: add ui for empty state
   return (
     <div className="space-y-4">
@@ -24,7 +31,9 @@ const Events: NextPage = () => {
           <h1 className="scroll-m-20 text-2xl font-semibold tracking-tight">
             Your events
           </h1>
-          <p className="text-muted-foreground">Helpful description here...</p>
+          <p className="text-muted-foreground">
+            Your upcoming and past events all in one place
+          </p>
         </div>
         <Link href="/events/new" className={buttonVariants()}>
           <PlusCircledIcon className="mr-2 h-4 w-4" /> Create event
@@ -32,51 +41,7 @@ const Events: NextPage = () => {
       </div>
       <Separator />
       <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
-        {[1, 2, 3].map((i) => (
-          // TODO: move to a separate component when this is used in another page
-          <Card key={i}>
-            <CardHeader>
-              <CardTitle>
-                {`Event ${i}`}
-                {/* TODO: change condition to past events */}
-                {i > 2 && (
-                  <Badge variant="secondary" className="ml-2">
-                    Finished
-                  </Badge>
-                )}
-              </CardTitle>
-              <CardDescription>Short description of event</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <ul className="ml-6 list-disc text-sm">
-                <li>
-                  <span className="font-semibold">Where:&nbsp;</span>
-                  Jln. D.I Panjaitan km. 9
-                </li>
-                <li>
-                  <span className="font-semibold">When:&nbsp;</span>
-                  {new Date().toLocaleString("en-uk", {
-                    weekday: "short", // short weekday name
-                    day: "2-digit", // day of the month (leading zero if single digit)
-                    month: "short", // abbreviated month name
-                    year: "numeric", // 4-digit year
-                    hour: "2-digit", // hours (24-hour format, leading zero if single digit)
-                    minute: "2-digit", // minutes (leading zero if single digit)
-                    hour12: false, // use 24-hour format
-                  })}
-                </li>
-              </ul>
-            </CardContent>
-            <CardFooter>
-              <Link
-                href={`/events/${i}/overview`}
-                className={cn(buttonVariants({ variant: "outline" }), "w-full")}
-              >
-                View event
-              </Link>
-            </CardFooter>
-          </Card>
-        ))}
+        {events?.map((event, i) => <EventCard key={i} event={event} />)}
       </div>
     </div>
   )
---
+++ src/components/event-card.tsx
@@ -0,0 +1,115 @@
+import { CalendarIcon } from "@radix-ui/react-icons"
+import Link from "next/link"
+
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Badge } from "@/components/ui/badge"
+import { Button, buttonVariants } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+import { Database } from "@/lib/types/supabase"
+
+// TODO: make a script to autogenerate friendlier types? e.g. Event
+// https://github.com/orgs/supabase/discussions/13364
+type Event = Database["public"]["Tables"]["events"]["Row"]
+interface EventCardProps {
+  event: Event
+}
+
+const hasEventPast = (dt: string) => new Date(dt) < new Date()
+
+const formatDatetime = (dt: string) =>
+  new Date(dt).toLocaleString("en-uk", {
+    weekday: "short", // short weekday name
+    day: "2-digit", // day of the month (leading zero if single digit)
+    month: "short", // abbreviated month name
+    year: "numeric", // 4-digit year
+    hour: "2-digit", // hours (24-hour format, leading zero if single digit)
+    minute: "2-digit", // minutes (leading zero if single digit)
+    hour12: false, // use 24-hour format
+  })
+
+const formatSlug = (event: Event) => {
+  const safeName = event.name
+    .toLowerCase()
+    .replaceAll(/[^\w\d\s-]/g, "")
+    .replaceAll(/\s+/g, "-")
+  return `e${event.event_id}-${safeName}`
+}
+
+export const EventCard = ({ event }: EventCardProps) => {
+  if (!event) return null
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>
+          {event.name}
+          {hasEventPast(event.event_datetime) && (
+            <Badge variant="secondary" className="ml-2">
+              Finished
+            </Badge>
+          )}
+          {!event.is_verified && (
+            <Badge variant="secondary" className="ml-2">
+              Pending confirmation
+            </Badge>
+          )}
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <ul className="text-sm space-y-2 [&>li]:flex [&>li]:items-center [&>li]:gap-2">
+          <li>
+            {/* radix doesn't have the map pin icon unfortunately */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.85}
+              stroke="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+              />
+            </svg>
+            {event.location}
+          </li>
+          <li>
+            <CalendarIcon aria-label="date & time" className="h-5 w-5" />
+            {formatDatetime(event.event_datetime)}
+          </li>
+        </ul>
+      </CardContent>
+      <CardFooter>
+        {/* TODO: create a format slug function, e.g. formatSlug(event) */}
+        {event.is_verified ? (
+          <Link
+            href={`/events/${formatSlug(event)}/overview`}
+            className={cn(buttonVariants({ variant: "secondary" }), "w-full")}
+          >
+            View event
+          </Link>
+        ) : (
+          // because cursor-not-allowed and pointer-events-none can't coexist
+          <div className="w-full hover:cursor-not-allowed">
+            <Button disabled variant="outline" className="w-full">
+              View event
+            </Button>
+          </div>
+        )}
+      </CardFooter>
+    </Card>
+  )
+}
