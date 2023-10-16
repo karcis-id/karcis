8286265d268b2cf98cf00eea1b9ec1f8794084ce
Ryan Martin <rmrt1n@users.noreply.github.com>
2023-10-16 16:48:20 +0700

    build: add sort imports prettier plugin

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,9 @@
 const path = require("path")
 
 const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(" --file ")}`
+  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(" --file ")}`
 
 module.exports = {
   "*.{js,jsx,ts,tsx}": ["npm run format", buildEslintCommand],
+  "*.{yaml,json}": ["npm run format"],
 }
